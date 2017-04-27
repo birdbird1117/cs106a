91c26e99a2735ad577ae99a854a1777ca75cc7c5
birdbird1117 <dapeng.han.1989@gmail.com>
2017-04-27 22:17:01 +0200

    temp ~

--- Assignment2/QuadraticFormula.java
+++ Assignment2/QuadraticFormula.java
@@ -5,6 +5,16 @@ import acm.program.*;
 
 public class QuadraticFormula extends ConsoleProgram {
 	public void run() {
+		int a = readInt("Enter a: ");
+		int b = readInt("Enter b: ");
+		int c = readInt("Enter c: ");
 		
+		double y = Math.sqrt(b);
+		double discrim = y - 4*a*c;
+		//if ( discrim > 0)
+			
+		println("test");
 	}
+	
+	
 }
