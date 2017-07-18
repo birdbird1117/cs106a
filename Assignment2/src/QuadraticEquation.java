98e3f3fe0aab16892090dfba74f4a1aee67f3a47
birdbird1117 <dapeng.han.1989@gmail.com>
2017-07-18 23:19:53 +0200

    test with set in pdf

--- Assignment2/src/QuadraticEquation.java
+++ Assignment2/src/QuadraticEquation.java
@@ -1,9 +1,27 @@
-// TODO: comment this program
+/* 
+ * Write an interactive ConsoleProgram named QuadraticEquation that finds real roots of a quadratic equation. 
+ * 
+ * You may assume that the user doesn't enter 0 as their value for a. 
+ * Aside from the above restriction, the values of a, b, and c can be any integers. 
+ * You should not do any rounding of real numbers in your output.
+*/
 
 import acm.program.*;
 
 public class QuadraticEquation extends ConsoleProgram {
 	public void run() {
-		// TODO: finish this program
+		println("CS 106A Quadratic Solver!");
+		int a = readInt("Enter a: ");
+		int b = readInt("Enter b: ");
+		int c = readInt("Enter c: ");
+		
+		double discriminant = b*b - 4*a*c;
+		
+		if (discriminant < 0)
+			println("No real roots");
+		else if (discriminant == 0)
+			println("One root: " + -b/2/a);
+		else 
+			println("Two roots: " + (-b+Math.sqrt(discriminant))/2/a + " and " + (-b-Math.sqrt(discriminant))/2/a);
 	}
 }
