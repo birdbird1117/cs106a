1508fdedeae4f3f3132f94761b09caea6584cf1d
birdbird1117 <dapeng.han.1989@gmail.com>
2017-08-09 23:38:14 +0200

    finish head and tail

--- Assignment2/src/Rocket.java
+++ Assignment2/src/Rocket.java
@@ -12,10 +12,16 @@ public class Rocket extends ConsoleProgram {
 
 	public void run() {
 		printHeadTail();
+		printBodyLine();
+		printUpperBody();
+		printLowerBody();
+		printBodyLine();
+		printHeadTail();		
 	}
 	
 	public void printHeadTail() {
 		for (int i = 0; i < SIZE; i++) {
+			print(" ");
 			for (int j = 0; j < SIZE-i-1; j++)
 				print(" ");
 			for (int j = 0; j < i+1; j++)
@@ -28,6 +34,49 @@ public class Rocket extends ConsoleProgram {
 		}
 	}
 	
+	public void printBodyLine() {
+		print("+");
+		for (int i = 0; i < SIZE*2; i++) {
+			print("=");
+		}
+		print("+");
+		println();
+	}
 	
+	public void printUpperBody() {
+		for (int i = 0; i < SIZE; i++) {
+			print("|");
+			for (int j = 0; j < SIZE-i-1; j++)
+				print(".");
+			for (int j = 0; j < (i+1)*2; j++) {
+				if (j%2 == 0)
+					print("/");
+				else
+					print("\\");
+			}
+			for (int j = 0; j < SIZE-i-1; j++)
+				print(".");	
+			print("|");
+			println();
+		}
+	}
+	
+	public void printLowerBody() {
+		for (int i = SIZE-1; i >= 0; i--) {
+			print("|");
+			for (int j = 0; j < SIZE-i-1; j++)
+				print(".");
+			for (int j = 0; j < (i+1)*2; j++) {
+				if (j%2 == 0)
+					print("\\");
+				else
+					print("/");
+			}
+			for (int j = 0; j < SIZE-i-1; j++)
+				print(".");	
+			print("|");
+			println();
+		}
+	}
 	
 }
