d87aab64cc5dadcdf80aea29d91b83c38abb2bf9
birdbird1117 <dapeng.han.1989@gmail.com>
2017-08-23 22:51:43 +0200

    add assigment 4

---
+++ Assignment4/src/Breakout.java
@@ -0,0 +1,21 @@
+// TODO: comment this program
+
+import acm.graphics.*;     // GOval, GRect, etc.
+import acm.program.*;      // GraphicsProgram
+import acm.util.*;         // RandomGenerator
+import java.awt.*;         // Color
+import java.awt.event.*;   // MouseEvent
+
+public class Breakout extends BreakoutProgram {
+
+	public void run() {
+		// Set the window's title bar text
+		setTitle("CS 106A Breakout");
+
+		// Set the canvas size.  In your code, remember to ALWAYS use getWidth()
+		// and getHeight() to get the screen dimensions, not these constants!
+		setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
+		
+		// TODO: finish this program
+	}
+}
