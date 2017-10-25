705c05be822e6f9890eae27f09743b6d0af8c8ef
birdbird1117 <dapeng.han.1989@gmail.com>
2017-10-26 00:31:26 +0200

    one line of bricks

--- Assignment4/src/Breakout.java
+++ Assignment4/src/Breakout.java
@@ -8,6 +8,31 @@ import java.awt.event.*;   // MouseEvent
 
 public class Breakout extends BreakoutProgram {
 
+	public void setOneBrickLine(int RowNumber, int InitYLocation) {
+		double InitXLocationLeft;
+		double InitXLocationRight;
+		
+		if (NBRICK_COLUMNS % 2 == 0) {
+			InitXLocationLeft = CANVAS_WIDTH/2-BRICK_SEP/2-BRICK_WIDTH;
+			InitXLocationRight = CANVAS_WIDTH/2+BRICK_SEP/2;
+			for (int i = 0; i < NBRICK_COLUMNS/2; i++) {
+				GRect rect= new GRect(InitXLocationLeft-(BRICK_SEP+BRICK_WIDTH)*i, InitYLocation, BRICK_WIDTH, BRICK_HEIGHT);
+				rect.setFilled(true);
+				rect.setColor(Color.RED);
+				add(rect);
+				rect= new GRect(InitXLocationRight+(BRICK_SEP+BRICK_WIDTH)*i, InitYLocation, BRICK_WIDTH, BRICK_HEIGHT);
+				rect.setFilled(true);
+				rect.setColor(Color.RED);
+				add(rect);
+			}
+		}
+	}
+	
+//	public void setBricks() {
+//		
+//	}
+	
+	
 	public void run() {
 		// Set the window's title bar text
 		setTitle("CS 106A Breakout");
@@ -17,5 +42,6 @@ public class Breakout extends BreakoutProgram {
 		setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
 		
 		// TODO: finish this program
+		setOneBrickLine(1, 20);
 	}
 }
