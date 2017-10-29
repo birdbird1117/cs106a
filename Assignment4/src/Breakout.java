d00c688ebb265e43536c1778bde44b2d8e4857c0
birdbird1117 <dapeng.han.1989@gmail.com>
2017-10-29 12:52:30 +0100

    stage 1 bricks

--- Assignment4/src/Breakout.java
+++ Assignment4/src/Breakout.java
@@ -8,7 +8,23 @@ import java.awt.event.*;   // MouseEvent
 
 public class Breakout extends BreakoutProgram {
 
-	public void setOneBrickLine(int RowNumber, int InitYLocation) {
+	public void setBricksColor(int RowNumber, GRect rect) {
+		switch (RowNumber % 10) {
+			case 0: ;
+			case 1: rect.setColor(Color.RED); break;
+			case 2: ;
+			case 3: rect.setColor(Color.ORANGE); break;
+			case 4: ;
+			case 5: rect.setColor(Color.YELLOW); break;
+			case 6: ;
+			case 7: rect.setColor(Color.GREEN); break;
+			case 8: ;
+			case 9: rect.setColor(Color.CYAN); break;
+			default: break;
+		}
+	}
+
+	public void setOneBrickLine(int RowNumber, double InitYLocation) {
 		double InitXLocationLeft;
 		double InitXLocationRight;
 		
@@ -18,19 +34,25 @@ public class Breakout extends BreakoutProgram {
 			for (int i = 0; i < NBRICK_COLUMNS/2; i++) {
 				GRect rect= new GRect(InitXLocationLeft-(BRICK_SEP+BRICK_WIDTH)*i, InitYLocation, BRICK_WIDTH, BRICK_HEIGHT);
 				rect.setFilled(true);
-				rect.setColor(Color.RED);
+//				rect.setColor(Color.RED);
+				setBricksColor(RowNumber, rect);
 				add(rect);
 				rect= new GRect(InitXLocationRight+(BRICK_SEP+BRICK_WIDTH)*i, InitYLocation, BRICK_WIDTH, BRICK_HEIGHT);
 				rect.setFilled(true);
-				rect.setColor(Color.RED);
+//				rect.setColor(Color.RED);
+				setBricksColor(RowNumber, rect);
 				add(rect);
 			}
 		}
 	}
 	
-//	public void setBricks() {
-//		
-//	}
+	public void setBricks() {
+		double InitYLocation = BRICK_Y_OFFSET;
+		for (int i = 0; i < NBRICK_ROWS; i++) {
+			setOneBrickLine(i, InitYLocation);
+			InitYLocation = InitYLocation + BRICK_HEIGHT + BRICK_SEP;
+		}
+	}
 	
 	
 	public void run() {
@@ -42,6 +64,7 @@ public class Breakout extends BreakoutProgram {
 		setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
 		
 		// TODO: finish this program
-		setOneBrickLine(1, 20);
+		//setOneBrickLine(1, 20);
+		setBricks();
 	}
 }
