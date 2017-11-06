caf729faf22df5145ea7c00de49b0d7ddadf0191
birdbird1117 <dapeng.han.1989@gmail.com>
2017-11-06 20:59:21 +0100

    blur, looks like can be refactored :(

--- Assignment5/src/ImageShopAlgorithms.java
+++ Assignment5/src/ImageShopAlgorithms.java
@@ -114,7 +114,8 @@ public class ImageShopAlgorithms implements ImageShopAlgorithmsInterface {
 		int[][] pixelsRotateLeft = new int[cols][rows];
 
 		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols ; j++ ) {				
+			for (int j = 0; j < cols ; j++ ) {
+			// avoid negtive mod 				
 				pixelsRotateLeft[((i+dy)%rows+rows)%rows][((j+dx)%cols+cols)%cols] = pixels[i][j];
 			}
 		}
@@ -123,8 +124,67 @@ public class ImageShopAlgorithms implements ImageShopAlgorithmsInterface {
 		return image;		
 	}
 
+	private int averagePixels(int... pixels) {
+		int red = 0;
+		int green = 0;
+		int blue = 0;
+		for (int i = 0; i < pixels.length; i++){
+			red += GImage.getRed(pixels[i]);
+			green += GImage.getGreen(pixels[i]);
+			blue += GImage.getBlue(pixels[i]);
+		}
+		red = red / pixels.length;
+		green = green/pixels.length;
+		blue = blue/pixels.length;
+		return 	GImage.createRGBPixel(red, green, blue);
+	}
+
+
+///// private void printScores(Player... players) {
+//     for (int i = 0; i < players.length; ++i) {
+//         Player player = players[i];
+//         String name   = player.getName();
+//         int    score  = player.getScore();
+//         // Print name and score followed by a newline
+//         System.out.format("%s: %d%n", name, score);
+//     }
+// }
 	public GImage blur(GImage source) {
-		// TODO
-		return null;
+		int[][] pixels = source.getPixelArray();
+		int rows = pixels.length;
+		int cols = pixels[0].length;
+		int[][] pixelsBlur = new int[rows][cols];
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols ; j++ ) {				
+				if((i==0)&&(j==0)) {
+					pixelsBlur[i][j] = averagePixels(pixels[i][j],pixels[i][j+1],pixels[i+1][j],pixels[j+1][j+1]);
+				} else if((i==rows-1)&&(j==0)) {
+					pixelsBlur[i][j] = averagePixels(pixels[i][j] , pixels[i][j+1] , pixels[i-1][j] , pixels[i-1][j+1]);					
+				} else if((i==rows-1)&&(j==cols-1)) {
+					pixelsBlur[i][j] = averagePixels(pixels[i][j] , pixels[i][j-1] , pixels[i-1][j] , pixels[i-1][j-1]);					
+				} else if((i==0)&&(j==cols-1)) {
+					pixelsBlur[i][j] = averagePixels(pixels[i][j] , pixels[i][j-1] , pixels[i+1][j] , pixels[i+1][j-1]);					
+				} else if((i==0)&&(j==cols-1)) {
+					pixelsBlur[i][j] = averagePixels(pixels[i][j] , pixels[i][j-1] , pixels[i+1][j] , pixels[i+1][j-1]);					
+				} else if(i==0) {
+					pixelsBlur[i][j] = averagePixels(pixels[i][j] , pixels[i][j-1] , pixels[i][j+1] , pixels[i+1][j-1] , pixels[i+1][j] , pixels[i+1][j+1]);
+				} else if(i==rows-1) {
+					pixelsBlur[i][j] = averagePixels(pixels[i][j] , pixels[i][j-1] , pixels[i][j+1] , pixels[i-1][j-1] , pixels[i-1][j] , pixels[i-1][j+1]);
+				} else if(j==0) {
+					pixelsBlur[i][j] = averagePixels(pixels[i][j] , pixels[i-1][j] , pixels[i+1][j] , pixels[i+1][j+1] , pixels[i][j+1] , pixels[i-1][j+1]);
+				} else if(j==cols-1) {
+					pixelsBlur[i][j] = averagePixels(pixels[i][j] , pixels[i-1][j] , pixels[i+1][j] , pixels[i+1][j-1] , pixels[i][j-1] , pixels[i-1][j-1]);
+				} else {
+					pixelsBlur[i][j] = averagePixels(pixels[i][j] , pixels[i-1][j-1] , pixels[i-1][j] , pixels[i-1][j+1] 
+						, pixels[i][j-1] , pixels[i][j+1] , pixels[i+1][j-1] , pixels[i+1][j] , pixels[i+1][j+1]);
+
+				}
+
+			}
+		}
+		
+		GImage image = new GImage(pixelsBlur);
+		return image;				
 	}
 }
