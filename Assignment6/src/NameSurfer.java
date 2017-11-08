87ad7be25d78bb3f8abd767e8e2ac12194a64bb6
birdbird1117 <dapeng.han.1989@gmail.com>
2017-11-08 20:38:54 +0100

    milestone 1

--- Assignment6/src/NameSurfer.java
+++ Assignment6/src/NameSurfer.java
@@ -6,6 +6,24 @@ import javax.swing.*;
 
 public class NameSurfer extends Program implements NameSurferConstants {
 
-	// TODO: finish this program
+	public void init() {
+	    JLabel nameLabel = new JLabel("Name: ");
+		add(nameLabel, NORTH);
+		JTextField nameField = new JTextField(TEXT_FIELD_WIDTH);
+		add(nameField, NORTH);
+		JButton graphButton = new JButton("Graph");
+		add(graphButton, NORTH);
+		JButton clearButton = new JButton("Clear");
+		add(clearButton, NORTH);
+		addActionListeners();
+		nameField.addActionListener(this);
+		nameField.setActionCommand("Graph");
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		println(event.getActionCommand());
+		if(event.getActionCommand().equals("Graph"))
+		println("graphButton");	
+	}
 
 }
