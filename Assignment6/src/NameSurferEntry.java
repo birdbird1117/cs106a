8801da7f24927092feef9e547cf5895320f6d89b
birdbird1117 <dapeng.han.1989@gmail.com>
2017-11-08 21:23:38 +0100

    milestone 2

--- Assignment6/src/NameSurferEntry.java
+++ Assignment6/src/NameSurferEntry.java
@@ -4,30 +4,46 @@ import java.util.*;
 
 public class NameSurferEntry implements NameSurferConstants {
 
+	//private HashMap<String, Integer[]> nameData = new HashMap<>();
+	private String name;
+	private Integer[] nameRank = new Integer[11];
 	// TODO: comment this constructor
 	public NameSurferEntry(String dataLine) {
-		// TODO: implement this constructor
+		String[] dataLineParts = dataLine.split(" ");
+		name = dataLineParts[0];
+		for(int i = 1; i <12; i++) {
+			//System.out.println(dataLineParts[i]);
+			nameRank[i-1] = Integer.valueOf(dataLineParts[i]);
+		}
 	}
 
 	// TODO: comment this method
 	public String getName() {
 		// TODO: implement this method
 
-		return "";	// remove this line
+		return name;	
 	}
 
 	// TODO: comment this method
 	public int getRank(int decadesSince1900) {
 		// TODO: implement this method
 
-		return 0;	// remove this line
+		return nameRank[decadesSince1900];	
 	}
 
 	// TODO: comment this method
 	public String toString() {
 		// TODO: implement this method
 
-		return "??????";	// remove this line
+		return name+" "+Arrays.toString(nameRank);	// remove this line
+	}
+
+	public static void main(String[] args) {
+		String test = "Sam 1 2 3 4 5 6 7 8 9 10 11";
+		NameSurferEntry entryTest = new NameSurferEntry(test);
+		System.out.println(entryTest.getName());
+		System.out.println(entryTest.getRank(0));
+		System.out.println(entryTest.toString());
 	}
 }
 
