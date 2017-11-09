14a75eb7d4b44d51bd0f87623a983d4e9db7d1a9
birdbird1117 <dapeng.han.1989@gmail.com>
2017-11-09 18:18:30 +0100

    milestone3 database

--- Assignment6/src/NameSurferDatabase.java
+++ Assignment6/src/NameSurferDatabase.java
@@ -4,17 +4,34 @@ import java.io.*;
 import java.util.*;
 
 public class NameSurferDatabase implements NameSurferConstants {
-	
+	HashMap<String, String> nameDataBase = new HashMap<String, String>();
 	// TODO: comment this constructor
 	public NameSurferDatabase(String filename) {
 		// TODO: fill this in
+		try{
+			Scanner input = new Scanner(new File(filename));
+			while(input.hasNextLine()){
+				String oneNameData = input.nextLine();
+				String[] oneDataLineParts = oneNameData.split(" ");
+				nameDataBase.put(oneDataLineParts[0], oneNameData);
+			}
+			input.close();
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("Error reading file: " + fnfe);
+		}
 	}
 	
 	// TODO: comment this method
 	public NameSurferEntry findEntry(String name) {
 		// TODO: implement this method
-
-		return null;	// remove this line
+		String oneNameData = nameDataBase.get(name);
+		NameSurferEntry oneNameSurferEntry = new NameSurferEntry(oneNameData);		
+		return oneNameSurferEntry;	// remove this line
+	}
+	
+	public static void main (String[] args) {
+		NameSurferDatabase nameDataBase = new NameSurferDatabase("./res/names-data.txt");
+		System.out.println(nameDataBase.findEntry("Sam").toString());
 	}
 }
 
